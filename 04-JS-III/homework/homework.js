ec7450650c82840d7c3f03d20eeb304b10e2dab3
Ivanfer92 <ivanzequielf19@gmail.com>
2021-11-02 22:46:40 -0300

    first commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.lenght -1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.lenght;
 }
 
 
@@ -23,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var nuevoArray=[];
+  for(var i=0; i<array.lenght; i++){
+    nuevoArray[i]= array[i]+1;
+  }
+  return nuevoArray;
 }
 
 
